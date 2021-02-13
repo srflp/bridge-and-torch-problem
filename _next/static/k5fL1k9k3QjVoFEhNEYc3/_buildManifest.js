525797e62dfbe8463cdacea13428df3a40a2a826
srflp <sauer.filip@gmail.com>
2021-02-13 17:03:39 +0000

    Deploying to gh-pages from  @ afe77951aaecd5bce2cac17288fde03be85dc32a 🚀

---
+++ _next/static/k5fL1k9k3QjVoFEhNEYc3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-4799488db64ff678ce8d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ef4d554b027b0ba34875.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
